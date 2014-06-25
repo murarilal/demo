97ec80040fdb6121ac1467f2ca72defd782b5b29
NTF-32 <ntf-32@NTF-32.(none)>
2014-06-25 10:46:27 +0530

    my first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog1::Application.config.secret_token = 'c2f599457a4d2f664d42935865bc8575e7b782de42520a36ce7e39bf9c8b141b6b1bbda6d09f913c9d702f18ac7676d7f680cb9ad4818089aa3990a053fb35b8'
